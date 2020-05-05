5b9847bab003857bdf70482ebaa7d5addf1ce326
ahmad alfan <alfanjuni@gmail.com>
2020-05-06 04:18:44 +0700

    change class to fungsional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-	static contextType = ThemeContext;
-	render() {
-		const { isLightTheme, light, dark } = this.context;
-		const theme = isLightTheme ? light : dark;
-		return (
-			<div
-				className="book-list"
-				style={{ background: theme.bg, color: theme.syntax }}
-			>
-				<ul>
-					<li style={{ background: theme.ui }}>the way of king</li>
-					<li style={{ background: theme.ui }}>the name of the wind</li>
-					<li style={{ background: theme.ui }}>the final empire</li>
-				</ul>
-			</div>
-		);
-	}
-}
+//pake class component
+// class BookList extends Component {
+// 	static contextType = ThemeContext;
+// 	render() {
+// 		const { isLightTheme, light, dark } = this.context;
+// 		const theme = isLightTheme ? light : dark;
+// 		return (
+// 			<div
+// 				className="book-list"
+// 				style={{ background: theme.bg, color: theme.syntax }}
+// 			>
+// 				<ul>
+// 					<li style={{ background: theme.ui }}>the way of king</li>
+// 					<li style={{ background: theme.ui }}>the name of the wind</li>
+// 					<li style={{ background: theme.ui }}>the final empire</li>
+// 				</ul>
+// 			</div>
+// 		);
+// 	}
+// }
+
+//pake fungsional dan useContext hooks
+const BookList = () => {
+	const { isLightTheme, light, dark } = useContext(ThemeContext);
+	const theme = isLightTheme ? light : dark;
+	return (
+		<div
+			className="book-list"
+			style={{ background: theme.bg, color: theme.syntax }}
+		>
+			<ul>
+				<li style={{ background: theme.ui }}>the way of king</li>
+				<li style={{ background: theme.ui }}>the name of the wind</li>
+				<li style={{ background: theme.ui }}>the final empire</li>
+			</ul>
+		</div>
+	);
+};
 
 export default BookList;
+
+// export default BookList;
